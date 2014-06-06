3e403c55f7eeeead49ad99555f93edff72b49fc9
Guz Alexander <kalimatas@gmail.com>
2014-06-06 20:05:19 +0400

    StateStack

---
+++ src/com/github/kalimatas/c05_States/Fonts.java
@@ -0,0 +1,5 @@
+package com.github.kalimatas.c05_States;
+
+public enum Fonts {
+    MAIN,
+}
---
+++ src/com/github/kalimatas/c05_States/States.java
@@ -0,0 +1,10 @@
+package com.github.kalimatas.c05_States;
+
+public enum States {
+    NONE,
+    TITLE,
+    MENU,
+    GAME,
+    LOADING,
+    PAUSE,
+}
